6b67acb4143e262ec79099035f5976a4d0607633
zhangjingxiang <jz3313@columbia.edu>
2020-11-25 17:05:38 +0800

    test

--- dm.py
+++ dm.py
@@ -2,8 +2,9 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
+#data preprocess
 topo = pd.read_csv('topo.txt', sep = '	', header=None, names=['linkid', 'downid'])
 attr = pd.read_csv('attr.txt', sep = '	', header=None, names=['linkid', 'length', 'direction', 'pathclass', 'speedclass',
                                                                    'LaneNume', 'speedlimit', 'level', 'width'])
 data0701 = pd.read_csv('20190701.txt', sep=';', header=None)
-
+data0701[0].str.split(' ', expand=True)
