7f10bf2f3543aa7db2f6ddd5f3584026ab8057d7
zhangjingxiang <jz3313@columbia.edu>
2020-11-30 15:40:44 +0800

    Decision Tree

--- dm.py
+++ dm.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import lightgbm as lgb
 import numpy as np
 import copy
 import re
@@ -60,31 +61,50 @@ def loadtradata(file, attr, topo_dict):
     return data
 
 if __name__ == '__main__':
-
+    # Load Data
     attr, topo_dict = loaddata()
     data = pd.DataFrame()
-    for i in range(1, 10):
+    for i in range(1, 31):
         filename = 20190700 + i
         strfile = str(filename) + '.txt'
         dataset = loadtradata(strfile, attr, topo_dict)
         df1 = dataset.loc[dataset['label'] == 1]
         df2 = dataset.loc[dataset['label'] == 2]
         df3 = dataset.loc[dataset['label'] == 3]
-        num = df1.shape[0]//2
+        num = df1.shape[0]//4
         random_df1 = df1.sample(n=num, random_state=3)
         df = pd.concat([random_df1, df2, df3])
         data = data.append(df)
-    print(data.head())
-    X_train = data.iloc[:, 2:]
-    Y_train = data.iloc[:, 1]
-
+    print(data.describe())
+    print(data.info())
+    # Shuffle dataset
+    data = data.sample(frac=1, random_state=1)
+    # Split data into train data and test data
+    num = data.shape[0]
+    X_train = data.iloc[:int(num*0.8), 2:]
+    Y_train = data.iloc[:int(num*0.8), 1]
+    X_crossval = data.iloc[int(num*0.8):, 2:]
+    Y_crossval = data.iloc[int(num*0.8):, 1]
     test = loadtradata('20190801_testdata.txt', attr, topo_dict)
     X_test = test.iloc[:,2:]
-
+    """
     # Decision Tree
     treeclf = tree.DecisionTreeClassifier()
     treeclf.fit(X_train, Y_train)
+    y_crossval = treeclf.fit(X_crossval)
+    scores = f1_score(Y_crossval, y_crossval, average=None)
+    scores = scores[0] * 0.2 + scores[1] * 0.2 + scores[2] * 0.6
+    print(scores)
     y_pred = treeclf.predict(X_test)
+    """
+    # LightGBM
+    Y_train = np.array(Y_train) - 1
+    train_data = lgb.Dataset(X_train, label=Y_train)
+    validation_data = lgb.Dataset(X_crossval, label=Y_crossval)
+    params = {'learning_rate':0.1, 'lambda_l2':0.2, 'max_depth':8, 'objective':'multiclass', 'num_class':3}
+    gbm = lgb.train(params, train_data, valid_sets=[validation_data])
+    y_pred = gbm.predict(X_test)
+
     out = {'link':test['linkid'], 'current_slice_id':test['current_slice_id'], 'future_slice_id':test['future_slice_id'], 'label':y_pred}
     out = pd.DataFrame(out)
     out.to_csv('result.csv', index=False)
